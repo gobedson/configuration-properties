a538bcf931c688fe59df7edc73aa29db0fff5691
NORDNETDOM\gyebadokpo.ext <Obed1990>
2017-03-24 13:20:06 +0100

    First Commit

---
+++ src/main/java/com/example/oluniyin/ConfigurationPropertiesApplication.java
@@ -0,0 +1,18 @@
+package com.example.oluniyin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+@EnableConfigurationProperties
+public class ConfigurationPropertiesApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx=  SpringApplication.run(ConfigurationPropertiesApplication.class, args);
+		MyAppConfig config= (MyAppConfig) ctx.getBean("myAppConfig");
+		
+		System.out.println(config.toString());
+	}
+}
